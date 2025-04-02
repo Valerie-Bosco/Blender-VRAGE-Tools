843891d951c945cdeabaf2d51eb347c24f38f76d
quantum-unicorn <70114668+quantum-unicorn@users.noreply.github.com>
2025-04-02 19:47:50 +0200

    modified copyright

--- vrage_tools/__init__.py
+++ vrage_tools/__init__.py
@@ -1,5 +1,5 @@
 '''
-Blender VRAGE Tools, Copyright 2025, 
+VRAGE Tools, Copyright 2025, 
 Keen Software House s.r.o., Na Petynce 213/23b, Břevnov, 169 00 Praha 6, Czech Republic, Company number: 05264561
 
 Licensed under the Apache License, Version 2.0 (the "License");
@@ -9,7 +9,7 @@ You may obtain a copy of the License at
 http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software 
-(Blender VRAGE Tools) distributed under the License is distributed on an "AS IS" BASIS, 
+(VRAGE Tools) distributed under the License is distributed on an "AS IS" BASIS, 
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
 See the License for the specific language governing permissions and 
 limitations under the License.
